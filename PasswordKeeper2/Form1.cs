db88403a0ef2ed3770acfdc716e9122d71cedae7
tsmit <tsmit@DESKTOP-9C7PPUF>
2022-12-07 18:31:26 -0600

    Add project files.

---
+++ PasswordKeeper2/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PasswordKeeper2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        { }
+           public string password { set; get; }
+        }
+    }
+
